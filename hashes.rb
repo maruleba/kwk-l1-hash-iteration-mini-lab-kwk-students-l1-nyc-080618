199da5763e5ba5b59eb45f1a793d1ac2502ba581
maruleba <folaa1234@gmail.com>
2018-08-09 19:27:33 +0000

    automatically backed up by learn

---
+++ hashes.rb
@@ -0,0 +1,28 @@
+# my_hash = { "cats" => "4 legged animal"}
+
+# items = {}
+# items ["24K Magic CD"] = 15.98
+# # items["crocs"] = 35 
+
+
+# new_item = "potato"
+# item_price = 200
+# item [new_item] = item_price
+
+# puts items 
+
+shopping_list = ["apple", "banana", "carrot"]
+price = [1,2,3,]
+
+shopping_hash = {}
+counter = 0 
+
+shopping_list.each do |rochelle|
+shopping_hash [rochelle] = price[counter]
+counter += 1
+  
+end
+ puts shopping_hash.keys
+ puts shopping_hash.values
+
+
--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,15 +1,39 @@
 
 def create_olympics_hash
   # Implement this method so that it returns a hash with the data provided on README.md
+ 
+# city ["Sydney"], : 2000
+# city ["Athens"] : 2004
+# city ["Beijing"] : 2008
+# city ["London"] : 2012
+
+summer_olympics |city|
+summer_olympics do city {1,2,3,4}
+ puts "Sydney 2000"
+ puts "Athens 2004"
+ puts "Beijings 2008"
+ puts "London 2012"
+end
+
 end
 
 def add_a_key_value_pair
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
+
+
+
 end
+
+
+
+
 
 def iterate_through_hash
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
   # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
+
+
+
 end
 
 def iterate_through_keys
