29779536a6bb55fc3cc450415bffcb6947c03f33
maruleba <folaa1234@gmail.com>
2018-08-09 20:00:42 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -7,8 +7,7 @@ def create_olympics_hash
 olympics {:Sydney => "2000" ,
 :Athens => "2004" ,
 :Beijing => "2008",
-:London  => "2012"
-:Atlanta => "1996"}
+:London  => "2012"}
 end
 create_olympics_hash
 
